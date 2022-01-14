432f0aeb255a55f414b428288b9c17614314ff05
Robert Kommeter <kommeterrobert@gmail.com>
2022-01-14 23:30:52 +0100

    add home button

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import react, { useState } from 'react';
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import AddCollection from '../AddCollection/AddCollection'
+import AddCollection from '../AddCollection/AddCollection';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [open, setOpen] = useState(false)
   return (
     <AppBar position="static">
       <Toolbar>
+        <Link to="/">RARE-META</Link>
         <Button sx={{float: "right"}}variant="contained" onClick={() => setOpen(true)}>Add Collection</Button>
         <AddCollection open={open} setOpen={setOpen} />
       </Toolbar>
